8752dc71505141bd201b224f6c57300fd7314c98
Bartlomiej Wydmuch <bartekoffice1@gmail.com>
2023-06-14 21:11:39 +0200

    adding modyfications to the script

--- my_sql.sh
+++ my_sql.sh
@@ -1,35 +1,28 @@
 #!/bin/bash
 
-# Aktualizacja repozytoriów
+# Update package lists
 sudo apt-get update
 
-# Instalacja MySQL
-sudo DEBIAN_FRONTEND=noninteractive apt-get install -y mysql-server
+# Install MySQL Server in a Non-Interactive mode. Default root password will be "root"
+echo "mysql-server mysql-server/root_password password root" | sudo debconf-set-selections
+echo "mysql-server mysql-server/root_password_again password root" | sudo debconf-set-selections
+sudo apt-get -y install mysql-server
 
-# Ustawienie hasła dla użytkownika root
-mysql -u root <<-EOF
-ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'root_password';
-FLUSH PRIVILEGES;
-EOF
+# Make MySQL accept connections from outside
+sudo sed -i 's/bind-address/bind-address = 0.0.0.0#/' /etc/mysql/mysql.conf.d/mysqld.cnf
 
-# Utworzenie użytkownika zdalnego
-mysql -u root -proot_password <<-EOF
-CREATE USER 'remote_user'@'%' IDENTIFIED BY 'password';
-GRANT ALL PRIVILEGES ON *.* TO 'remote_user'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-EOF
+# Allow root access from any host
+sudo mysql -uroot -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-# Zmiana konfiguracji MySQL, aby umożliwić zdalne połączenia
-sudo sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mysql/mysql.conf.d/mysqld.cnf
+# Create a new user that can connect from outside
+sudo mysql -uroot -proot -e "CREATE USER 'user'@'%' IDENTIFIED BY 'password'; GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;"
 
-# Restart MySQL
-sudo systemctl restart mysql
+# Restart MySQL Service
+sudo service mysql restart
 
-# Generowanie pliku z danymi logowania
-echo "MySQL został zainstalowany." > mysql_credentials.txt
-echo "Dane logowania do użytkownika root:" >> mysql_credentials.txt
-echo "Użytkownik: root" >> mysql_credentials.txt
-echo "Hasło: root_password" >> mysql_credentials.txt
-echo "Dane logowania do użytkownika zdalnego:" >> mysql_credentials.txt
-echo "Użytkownik: remote_user" >> mysql_credentials.txt
-echo "Hasło: password" >> mysql_credentials.txt
+# Save credentials to a file
+echo "MySQL setup completed. Here are the credentials:
+Username: user
+Password: password" > mysql_credentials.txt
+
+echo "Credentials saved to mysql_credentials.txt"
